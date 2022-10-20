f5af7399ce4b63504897825a4a407d8bdbf18c3d
liangzhicheng1994 <113342878+liangzhicheng1994@users.noreply.github.com>
2022-10-20 22:10:04 +0800

    Add glog.

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,29 @@
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+
 #include <cstdint>
 #include <iostream>
 #include <memory>
 #include <string>
 
+void InitGlog(const char* argv0) {
+  FLAGS_log_dir = "../log";
+  FLAGS_alsologtostderr = true;
+  FLAGS_colorlogtostderr = true;
+  FLAGS_stderrthreshold = google::GLOG_INFO;
+  google::SetLogFilenameExtension(".log");
+  google::InitGoogleLogging(argv0);
+}
+
+void ShutdownGlog() {
+  google::FlushLogFiles(google::GLOG_INFO);
+  google::ShutdownGoogleLogging();
+}
+
 int main(int argc, char* argv[]) {
-  std::cout << "Hello world." << std::endl;
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  InitGlog(argv[0]);
+
+  ShutdownGlog();
   return 0;
 }
